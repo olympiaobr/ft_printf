ec08a577c36d49c164e7e4a51fc4cf9d44875340
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2023-12-04 08:49:44 +0100

    correction

--- srcs/ft_prtstr.c
+++ srcs/ft_prtstr.c
@@ -5,7 +5,7 @@ void ft_prtstr(char *str, int *len)
     int i;
 
     if (str == NULL)
-        ft_prtstr (len,"(null)");
+        ft_prtstr ("(null)", len);
     i = 0;
     while (str[i] != '\0')
     {
